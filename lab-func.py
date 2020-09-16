d625e6817d4a938d1d3f66a5fbc58e5e32bc0380
vqthang <vqthangqngai@gmail.com>
2020-09-16 11:32:26 +0700

    fix code

--- lab-func.py
+++ lab-func.py
@@ -1,8 +1,8 @@
 def chia2so(sobichia,sochia):
-    if ((sochia == 0) and (sobichia != 0)):
+    if (sochia == 0):
         print("Khong the chia cho 0")
     else:
         print("So bi chia: " + format(sobichia))
         print ("So chia: " + format(sochia))
         print("Ket qua: " + format(sobichia/sochia))
-chia2so(10,5)
+chia2so(0,0)
